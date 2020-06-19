548c104ab46558c06b539fb38031b77c6420078d
SedationH <sedationh@gmail.com>
2020-06-19 17:14:19 +0800

    🎉 搭建框架

---
+++ REAEME.md
@@ -0,0 +1,18 @@
+# 📝学习过程
+
+整个项目使用dva进行状态管理
+
+![DVA图解](https://zos.alipayobjects.com/rmsportal/PPrerEAKbIoDZYr.png)
+
+
+
+dva是对redux&redux-saga的封装，并使用model集中管理同步和异步action
+
+
+
+[非常好的入门课](https://dvajs.com/guide/introduce-class.html)
+
+
+
+在项目中的具体使用
+
---
+++ src/app.jsx
@@ -0,0 +1,66 @@
+import Taro, { Component } from '@tarojs/taro'
+import { Provider } from '@tarojs/redux'
+import dva from './dva';
+import models from './models';
+
+import Index from './pages/index'
+
+import './app.less'
+
+// 如果需要在 h5 环境中开启 React Devtools
+// 取消以下注释：
+// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
+//   require('nerv-devtools')
+// }
+
+const dvaApp = dva.createApp({
+  initialState: {},
+  models: models,
+  onError(e, dispatch) {
+    console.log('系统出错了!');
+    // dispatch(action("sys/error", e));
+  },
+});
+const store = dvaApp.getStore();
+
+class App extends Component {
+
+  config = {
+    pages: [
+      'pages/index/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
+    },
+    subpackages: [{
+      root: "packages",
+      name: "pack2",
+      pages: [
+        'pages/demo/index'
+      ],
+    }]
+  }
+
+  componentDidMount() { }
+
+  componentDidShow() { }
+
+  componentDidHide() { }
+
+  componentDidCatchError() { }
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render() {
+    return (
+      <Provider store={store}>
+        <Index />
+      </Provider>
+    )
+  }
+}
+
+Taro.render(<App />, document.getElementById('app'))
